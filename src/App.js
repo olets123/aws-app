bdcc156e10a352f4e4090fe53ed2be1f0222e926
olets123 <47143886+olets123@users.noreply.github.com>
2020-09-07 12:04:23 +0200

    added auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
@@ -8,10 +9,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello V2</h1>
-        
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default  withAuthenticator(App);
